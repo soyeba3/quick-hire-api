b5caf31ef8f3903869fbd199a3d6465a31282d39
soyeba3 <soyebchandani@gmail.com>
2026-03-04 11:43:08 +0600

    chore: project initialization and base config

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+  port: process.env.PORT || 5000,
+  database: {
+    url:
+      process.env.DATABASE_URL ||
+      "mysql://root:password@localhost:3306/quickhire",
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || "quickhire_secret_key",
+    expiresIn: "7d",
+  },
+  admin: {
+    email: "admin@quickhire.com",
+    password: "admin123",
+  },
+};
